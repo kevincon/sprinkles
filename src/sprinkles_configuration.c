e4fb00a58d4a4d954234e73e165eb81f0f78fa1d
Grégoire Sage <gregoire.sage@gmail.com>
2016-02-22 17:54:42 +0100

    Move to Clay + Enamel

--- src/sprinkles_configuration.c
+++ src/sprinkles_configuration.c
@@ -1,7 +1,7 @@
 #include "sprinkles_configuration.h"
 
-// Might switch away from using autoconfig in the future, hence the need for this wrapper file
-#include "autoconfig.h"
+// Might switch away from using enamel in the future, hence the need for this wrapper file
+#include "enamel.h"
 
 static SprinklesConfigurationChangedCallback s_sprinkles_configuration_changed_callback;
 static void *s_sprinkles_configuration_callback_context;
@@ -13,22 +13,19 @@ static void prv_update_sprinkles_configuration(SprinklesConfiguration *configura
   }
 
   *configuration = (SprinklesConfiguration) {
-    .background_color = getBackground_color(),
-    .hour_hand_color = getHour_hand_color(),
-    .minute_hand_color = getMinute_hand_color(),
-    .center_dot_color = getCenter_dot_color(),
-    .seconds_hand_enabled = getSeconds_hand_enabled(),
-    .seconds_hand_color = getSeconds_hand_color(),
-    .date_enabled = getDate_enabled(),
-    .date_background_color = getDate_background_color(),
-    .date_text_color = getDate_text_color(),
+    .background_color = get_background_color(),
+    .hour_hand_color = get_hour_hand_color(),
+    .minute_hand_color = get_minute_hand_color(),
+    .center_dot_color = get_center_dot_color(),
+    .seconds_hand_enabled = get_seconds_hand_enabled(),
+    .seconds_hand_color = get_seconds_hand_color(),
+    .date_enabled = get_date_enabled(),
+    .date_background_color = get_date_background_color(),
+    .date_text_color = get_date_text_color(),
   };
 }
 
 static void prv_app_message_inbox_received_handler(DictionaryIterator *iter, void *context) {
-  // Let autoconfig handle the received message
-  autoconfig_in_received_handler(iter, context);
-
   // Update the cached configuration
   prv_update_sprinkles_configuration(&s_current_sprinkles_configuration);
 
@@ -40,14 +37,10 @@ static void prv_app_message_inbox_received_handler(DictionaryIterator *iter, voi
 }
 
 void sprinkles_configuration_init(void) {
-  const uint32_t size_inbound = 300;
-  const uint32_t size_outbound = 0;
-  autoconfig_init(size_inbound, size_outbound);
+  enamel_init(0, 0);
+  enamel_register_custom_inbox_received(prv_app_message_inbox_received_handler);
 
   prv_update_sprinkles_configuration(&s_current_sprinkles_configuration);
-
-  // Register a custom received handler which in turn will call autoconfig's received handler
-  app_message_register_inbox_received(prv_app_message_inbox_received_handler);
 }
 
 void sprinkles_configuration_set_callback(SprinklesConfigurationChangedCallback callback,
@@ -61,5 +54,5 @@ SprinklesConfiguration *sprinkles_configuration_get_configuration(void) {
 }
 
 void sprinkles_configuration_deinit(void) {
-  autoconfig_deinit();
+  enamel_deinit();
 }
